1cd28b79a3b68fd67464a7683d464e177d22850c
BananaFlick-App <bananaflickapp@gmail.com>
2025-12-20 10:52:59 +0200

    Added files

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,21 +1,12 @@
+import { SUPABASE_URL } from "./src/config/env";
+
 export default {
   expo: {
-    name: 'BananaFlick',
-    slug: 'bananaflick',
-    scheme: 'bananaflick',
-    version: '1.0.0',
-    orientation: 'portrait',
-    icon: './src/assets/BananaFlick-logo.png',
-    userInterfaceStyle: 'automatic',
-    splash: {
-      backgroundColor: '#000000',
-    },
-    android: {
-      package: 'com.bananaflick.app',
-    },
+    name: "BananaFlick",
+    slug: "bananaflick",
     extra: {
-      supabaseUrl: 'https://kxpcwleogbutevqujqoz.supabase.co',
-      supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4cGN3bGVvZ2J1dGV2cXVqcW96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MzE2ODksImV4cCI6MjA4MTMwNzY4OX0.YaiJ119ZSEwAd5dc9mGvw9Rx06zN7kCDjd3Jz1RjkG4',
-    },
-  },
-};
+      SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+    }
+  }
+}
